c7b89f8770c21eff6572bc7d1c7390e0c5fe4783
hanbi <rain7i@naver.com>
2021-05-11 22:44:42 +0900

    feat: 로그인 핸들러

---
+++ src/main/java/com/real_estate/demo/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.real_estate.demo.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+        String errorMessage = "Access denied";
+        //response 상수 사용
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().append(errorMessage);
+    }
+}
---
+++ src/main/java/com/real_estate/demo/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,23 @@
+package com.real_estate.demo.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String errorMessage = "이메일이나 비밀번호가 잘못되었습니다";
+        //response 상수 사용
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().append(errorMessage);
+    }
+}
